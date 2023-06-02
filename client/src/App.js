110b83981ff0760b0df0001e77ba44d122d12616
DarkGladiator7 <ganeshkrishnadoss@gmail.com>
2023-06-02 09:16:37 +0530

    added footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
 function App() {
   return (
-   <div>
-    <Header/>
-    <Home/>
-    <Footer/>
-   </div>
-
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
   );
 }
 
