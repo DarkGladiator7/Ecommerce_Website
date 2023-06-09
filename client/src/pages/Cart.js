1130a3d89fdad63b855d607ff3cbb2a132123c61
DarkGladiator7 <ganeshkrishnadoss@gmail.com>
2023-06-09 10:05:50 +0530

    creating post req for stripe checkout

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -3,12 +3,20 @@ import { useSelector } from "react-redux";
 import { cartbg, userLogo } from "../assests";
 import CartItem from "../components/CartItem";
 import { ToastContainer, toast } from "react-toastify";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 const Cart = () => {
   const productData = useSelector((state) => state.bazar.productData);
   const userInfo = useSelector((state) => state.bazar.userInfo);
   const [totalAmt, setTotalAmt] = useState("");
   const [payNow, setPayNow] = useState(false);
 
+  const payment = async (token) => {
+    await axios.post("http.//localhost:8080/pay", {
+      amount: totalAmt * 100,
+      token: token,
+    });
+  };
   useEffect(() => {
     let price = 0;
 
@@ -31,7 +39,8 @@ const Cart = () => {
       <img className="w-full h-60 object-cover" src={cartbg} alt="cartbg" />
       <div className="max-w-screen-xl mx-auto py-20 flex">
         <CartItem />
-        <div className="w-1/3 bg-[#fafafa] py-6 px-4">
+
+        <div className="w-1/3 h-2/3 flex-none bg-[#fafafa]  py-6 px-4 ">
           <div className="flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
             <h2 className="text-2xl font-medium">Cart Totals</h2>
             <p className="flex items-center gap-4 text-base">
@@ -54,6 +63,19 @@ const Cart = () => {
           >
             Proceed to Checkout
           </button>
+          {payNow && (
+            <div>
+              <StripeCheckout
+                stripeKey="pk_test_51NGgvBSFXWj4YAa54KkjqCWEpxxLqHuu2WMrHVUGHsLD1zqbOvtjsSRpStIoUKwmsU7IcirYVzvwYWcdJMMYLGVW00LJGnpMD1"
+                name="DarkLight Online Shopping"
+                amount={totalAmt * 100}
+                label="Pay to DarkLight"
+                description={`Your Payment amount is $${totalAmt}`}
+                token={payment}
+                email={userInfo.email}
+              />
+            </div>
+          )}
         </div>
       </div>
       <ToastContainer
--- server/server.js
+++ server/server.js
@@ -1,56 +1,30 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// const cors = require("cors");
-// const PORT = process.env.PORT || 8080;
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT;
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// const admin = require("firebase-admin");
+const Stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-// // Initialize Firebase Admin SDK
-// const serviceAccount = require("./path/to/serviceAccountKey.json");
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://darklightindustries-966ec-default-rtdb.firebaseio.com",
-// });
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
 
-// app.use(express.json());
+app.post("/pay", async (req, res) => {
+  console.log(req.body.token);
+  await Stripe.charges.create({
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  });
+});
 
-// app.post(
-//   "https://darklightindustries-966ec-default-rtdb.firebaseio.com//products",
-//   (req, res) => {
-//     // Here, you can store the product details in Firebase
-//     const {
-//       _id,
-//       category,
-//       description,
-//       image,
-//       isNew,
-//       oldPrice,
-//       price,
-//       rating,
-//       title,
-//     } = req.body;
-
-//     // Firebase logic here
-//     const productRef = admin.database().ref("products/");
-//     const newProductRef = productRef;
-//     newProductRef.set({
-//       _id,
-//       category,
-//       description,
-//       image,
-//       isNew,
-//       oldPrice,
-//       price,
-//       rating,
-//       title,
-//     });
-
-//     // Return a response with the newly added product
-//     res.json({ message: "Product added successfully!", product: req.body });
-//   }
-// );
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+console.log("hello");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
